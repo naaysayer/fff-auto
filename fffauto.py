3a6c80a307aa6d90a6bdc16f77910e62ee0d2cef
naaysayer <note11781@gmail.com>
2024-02-02 18:29:00 +0400

    generate source and header file from json

--- fffauto.py
+++ fffauto.py
@@ -1,45 +1,86 @@
 #!/usr/bin/env python3
 
 import sys
+import pathlib
 import json
-import os
-
+from optparse import OptionParser
 
 OUT_NAME = "fakes"
 
 
-def fff_generate(json):
-    header = open(f"{OUT_NAME}.h", 'w')
-    source = open(f"{OUT_NAME}.cc", 'w')
+def generate_fake_function_declaration(name, return_type, args_types) -> str:
+    if return_type == 'void':
+        return f"DECLARE_FAKE_VOID_FUNC({name}, {','.join(map(str, args_types))});\n"
+    else:
+        return f"DECLARE_FAKE_VALUE_FUNC({return_type}, {name}, {','.join(map(str, args_types))});\n"
 
-    header.write("#ifndef __AUTO_FAKES_H__\n")
-    header.write("#define __AUTO_FAKES_H__\n")
 
+def generate_fake_function_definition(name, return_type, args_types) -> str:
+    if return_type == 'void':
+        return f"DEFINE_FAKE_VOID_FUNC({name}, {','.join(map(str, args_types))});\n"
+    else:
+        return f"DEFINE_FAKE_VALUE_FUNC({return_type}, {name}, {','.join(map(str, args_types))});\n"
+
+
+def generate_fake_functions_list(fakes_list) -> str:
+    fake_list_str = "#define FAKE_LIST(FAKE)\t\\\n"
+    for i, fake in enumerate(fakes_list):
+        if i == (len(fakes_list) - 1):
+            fake_list_str += f"\tFAKE({fake})\n"
+        else:
+            fake_list_str += f"\tFAKE({fake})\t\\\n"
+    return fake_list_str
+
+
+def generate_fake_function_header(json_data, header_name):
     fakes_list = []
 
-    for fdata in json:
-        name = fdata["function_name"]
-        return_type = fdata["return_value_type"]
-        argc = fdata["argc"]
-        args_types = fdata["arg_types"]
+    with open(header_name, 'w') as header:
+        header.write("#ifndef __AUTO_FAKES_H__\n")
+        header.write("#define __AUTO_FAKES_H__\n\n")
+        header.write("#include <fff.h>\n")
 
-        fakes_list.append(name)
+        header.write("\n/*** AUTO FAKES DECLARATION START ***/\n")
+        for fcall in json_data:
+            name = fcall["function_name"]
+            return_type = fcall["return_value_type"]
+            arg_types = fcall["arg_types"]
 
-    # print FAKE_LIST
-    header.write("#define FAKE_LIST(FAKE) \t\\\n")
-    for i in range(len(fakes_list)):
-        if i == (len(fakes_list) - 1):
-            header.write(f"\tFAKE({fakes_list[i]})\t\n")
-        else:
-            header.write(f"\tFAKE({fakes_list[i]})\t\\\n")
+            header.write(generate_fake_function_declaration(name, return_type, arg_types))
+            fakes_list.append(name)
 
-    # close include guards
-    header.write("#endif /* __AUTO_FAKES_H__*/\n")
+        header.write("/*** AUTO FAKES DECLARATION END ***/\n")
+
+        header.write("\n/*** AUTO FAKES LIST START ***/\n")
+        header.write(generate_fake_functions_list(fakes_list))
+        header.write("/*** AUTO FAKES LIST END ***/\n")
+
+        header.write("#endif /* __AUTO_FAKES_H__  */\n")
+
+
+def generate_fake_function_source(json_data, source_name):
+    with open(source_name, 'w') as source:
+        source.write("#define FFF_GCC_FUNCTION_ATTRIBUTES __attribute__((weak))\n")
+        source.write(f"#include \"{OUT_NAME}.h\"\n")
+        source.write("DEFINE_FFF_GLOBALS;\n")
+        source.write("\n/*** AUTO FAKES DEFINITION START ***/\n")
+
+        for fcall in json_data:
+            name = fcall["function_name"]
+            return_type = fcall["return_value_type"]
+            arg_types = fcall["arg_types"]
+
+            source.write(generate_fake_function_definition(name, return_type, arg_types))
+
+        source.write("/*** AUTO FAKES DEFINITION END ***/\n")
+
+
+def generate_fakes(json_data, out_dir):
+    generate_fake_function_header(json_data, pathlib.Path(out_dir, f"{OUT_NAME}.h"))
+    generate_fake_function_source(json_data, pathlib.Path(out_dir, f"{OUT_NAME}.cc"))
 
 
 def main():
-    from optparse import OptionParser
-
     parser = OptionParser("usage: %prog [-j JSON_FILE]")
     parser.add_option(
         "-j",
@@ -53,14 +94,19 @@ def main():
     parser.disable_interspersed_args()
     (opts, args) = parser.parse_args()
 
-    input = sys.stdin
+    input_file = sys.stdin
     if opts.json is not None:
-        input = open(opts.json, 'r')
+        input_file = open(opts.json, 'r')
 
-    json_data = json.load(input)
-
-    fff_generate(json_data)
-
+    try:
+        json_data = json.load(input_file)
+        generate_fakes(json_data, "")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}", file=sys.stderr)
+        sys.exit(1)
+    finally:
+        if opts.json is not None:
+            input_file.close()
 
 
 if __name__ == '__main__':
